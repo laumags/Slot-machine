ed1234a5cedc4ac714a339bed88850a7d76938eb
laumags <laumadz@gmail.com>
2022-06-18 16:35:31 +0300

    slot machine

--- slot-machine.php
+++ slot-machine.php
@@ -1,26 +1,27 @@
 <?php
-$wallet = 1000;
-$bet = (float)readline('Place your bet (0.10, 0.20, 0.30): ') . PHP_EOL;
-$symbols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
-$board = [
-    ['-', '-', '-'],
-    ['-', '-', '-'],
-    ['-', '-', '-'],
-    ['-', '-', '-'],
-    ['-', '-', '-'],
-];
-for ($i = 0; $i < 3; $i++) {
-    for ($j = 0; $j >= $i; $j++)
-    $random = array_rand($symbols, 2);
-    array_push($board[$i][$j], $random);
-}
+$wallet = 1000.00;
+while($wallet > 0) {
+    $choice = readline('Spin (yes/no)?: ');
+    if ($choice == "yes") {
+        $bet = (float)readline('Place your bet (0.10, 0.20, 0.30, 0.40, 0.50): ');
+        $symbols = ['A', 'B', 'C', 'D', 'E', 'F',
+            'A', 'B', 'C', 'D', 'E',
+            'A', 'B', 'C', 'D',
+            'A', 'B', 'C',
+            'A', 'B',
+            'A'];
+        $board = [];
 
-function displayBoard(array $board): void
-{
-    echo " {$board[0][0]} | {$board[0][1]} | {$board[0][2]} \n";
-    echo " {$board[1][0]} | {$board[1][1]} | {$board[1][2]} \n";
-    echo " {$board[2][0]} | {$board[2][1]} | {$board[2][2]} \n";
-    echo " {$board[3][0]} | {$board[3][1]} | {$board[3][2]} \n";
-    echo " {$board[4][0]} | {$board[4][1]} | {$board[4][2]} \n";
+        for ($i = 0; $i < 15; $i++) {
+            $random = array_rand($symbols);
+            $board[$i] = array_fill($i, 1, $symbols[$random]);
+        }
+            echo implode("", $board[0]) . " | " . implode("", $board[1]) . " | " . implode("", $board[2]) . " | " . implode("", $board[3]) . " | " . implode("", $board[4]) . "\n";
+            echo implode("", $board[5]) . " | " . implode("", $board[6]) . " | " . implode("", $board[7]) . " | " . implode("", $board[8]) . " | " . implode("", $board[9]) . "\n";
+            echo implode("", $board[10]) . " | " . implode("", $board[11]) . " | " . implode("", $board[12]) . " | " . implode("", $board[13]) . " | " . implode("", $board[14]) . "\n";
+            $wallet -= $bet;
+    } else {
+        echo "Your wallet balance is $wallet" . PHP_EOL;
+        break;
+    }
 }
-displayBoard($board);
