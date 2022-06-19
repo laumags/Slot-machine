3fcce92e172d2d7ca87cb887f59f13cbc98b8caf
laumags <laumadz@gmail.com>
2022-06-19 07:36:00 +0300

    slot machine

--- slot-machine.php
+++ slot-machine.php
@@ -1,6 +1,6 @@
 <?php
 $wallet = 1000.00;
-while($wallet > 0) {
+while ($wallet > 0) {
     $choice = readline('Spin (yes/no)?: ');
     if ($choice == "yes") {
         $bet = (float)readline('Place your bet (0.10, 0.20, 0.30, 0.40, 0.50): ');
@@ -10,16 +10,28 @@ while($wallet > 0) {
             'A', 'B', 'C',
             'A', 'B',
             'A'];
+        $symbolPrice = [
+            "A" => 0.01,
+            "B" => 0.02,
+            "C" => 0.03,
+            "D" => 0.04,
+            "E" => 0.05,
+            "F" => 0.06
+        ];
         $board = [];
 
         for ($i = 0; $i < 15; $i++) {
             $random = array_rand($symbols);
             $board[$i] = array_fill($i, 1, $symbols[$random]);
         }
-            echo implode("", $board[0]) . " | " . implode("", $board[1]) . " | " . implode("", $board[2]) . " | " . implode("", $board[3]) . " | " . implode("", $board[4]) . "\n";
-            echo implode("", $board[5]) . " | " . implode("", $board[6]) . " | " . implode("", $board[7]) . " | " . implode("", $board[8]) . " | " . implode("", $board[9]) . "\n";
-            echo implode("", $board[10]) . " | " . implode("", $board[11]) . " | " . implode("", $board[12]) . " | " . implode("", $board[13]) . " | " . implode("", $board[14]) . "\n";
-            $wallet -= $bet;
+        echo implode("", $board[0]) . " | " . implode("", $board[1]) . " | " . implode("",
+                $board[2]) . " | " . implode("", $board[3]) . " | " . implode("", $board[4]) . "\n";
+        echo implode("", $board[5]) . " | " . implode("", $board[6]) . " | " . implode("",
+                $board[7]) . " | " . implode("", $board[8]) . " | " . implode("", $board[9]) . "\n";
+        echo implode("", $board[10]) . " | " . implode("", $board[11]) . " | " . implode("",
+                $board[12]) . " | " . implode("", $board[13]) . " | " . implode("", $board[14]) . "\n";
+        $wallet -= $bet;
+
     } else {
         echo "Your wallet balance is $wallet" . PHP_EOL;
         break;
